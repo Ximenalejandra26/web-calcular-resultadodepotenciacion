997734f44b34201c87ad3de882362b889c4b3f5d
Ximena <Ximena.aleja021@outlook.com>
2024-10-21 13:00:29 -0500

    por ultimo se agrega document.write para que nos muestre el resultado mediante concatenacion

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let base = parseInt(prompt ("ingrese la base"))
 let exponente = parseInt(prompt ("ingrese la exponente"))
+
+
+let result = (base,exponente)
+
+
+document.write("el resultado obtenido es" , result)
