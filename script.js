64c3c0882adcb197047a5935275624599fba99da
Ximena <Ximena.aleja021@outlook.com>
2024-10-21 12:59:59 -0500

    se agrega variable result, para que nos ayude a calcular resultado

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 let base = parseInt(prompt ("ingrese la base"))
 let exponente = parseInt(prompt ("ingrese la exponente"))
-
-let result = (base,exponente)
-
-document.write("el resultado obtenido es" , result)
